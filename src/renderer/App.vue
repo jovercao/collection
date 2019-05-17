a640ce0d6fef29ab5e0f0898535a098170dbdd80
曹文杰 <jovercao@live.com>
2019-05-17 16:07:42 +0800

    - 创建项目

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'j-collection'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
